3d0249bbf6a1e6ab58449a9ef31122eaba0dcd01
Lucas Casemiro <141570809+lucascasemiro@users.noreply.github.com>
2024-07-05 09:53:24 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function submiting(){
+
+  
+    let nome = document.getElementById('name').value;
+    let sobrenome = document.getElementById('surname').value;
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('password').value;
+    let csenha = document.getElementById('confirm').value;
+    if(senha !== csenha){
+    alert("As senhas não condizem");
+    }
+
+  }
